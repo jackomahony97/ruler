5f314a255253fe2d6f18b621442e4c41e08ac066
Anh Mai <hoanganh.hoang44@gmail.com>
2020-02-20 20:51:36 +0000

    Opacity Adjust with Plus and Minus

---
+++ Source/Ruler/opacity.cs
@@ -0,0 +1,37 @@
+protected override void OnKeyDown(KeyEventArgs e)
+		{
+			switch (e.KeyCode)
+			{
+				case Keys.Right:
+				case Keys.Left:
+				case Keys.Up:
+				case Keys.Down:
+					this.HandleMoveResizeKeystroke(e);
+					break;
+
+				case Keys.Space:
+					this.ChangeOrientation();
+					break;
+
+				case Keys.ControlKey | Keys.S:
+					SetSizeHandler(this, EventArgs.Empty);
+					break;
+                case Key.Plus | Key.Minus:
+                    this.HandleOpacity(e);
+                    break;
+			}
+
+			base.OnKeyDown(e);
+		}
+    
+private void HandleOpacity(KeyEventArgs e)
+{
+    if (e.Keycode == Key.Plus)
+    {
+        rulerInfo.opacity*100;
+    }
+    else
+    {
+        rulerInfo.opacity/100;
+    }
+}
