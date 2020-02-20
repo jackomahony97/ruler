68391428eabccc01320d04a695dddc22a48d3cd9
Anh Mai <hoanganh.hoang44@gmail.com>
2020-02-20 20:53:02 +0000

    Format

--- Source/Ruler/opacity.cs
+++ Source/Ruler/opacity.cs
@@ -1,37 +1,37 @@
 protected override void OnKeyDown(KeyEventArgs e)
-		{
-			switch (e.KeyCode)
-			{
-				case Keys.Right:
-				case Keys.Left:
-				case Keys.Up:
-				case Keys.Down:
-					this.HandleMoveResizeKeystroke(e);
-					break;
+{
+    switch (e.KeyCode)
+    {
+        case Keys.Right:
+        case Keys.Left:
+        case Keys.Up:
+        case Keys.Down:
+            this.HandleMoveResizeKeystroke(e);
+            break;
 
-				case Keys.Space:
-					this.ChangeOrientation();
-					break;
+        case Keys.Space:
+            this.ChangeOrientation();
+            break;
 
-				case Keys.ControlKey | Keys.S:
-					SetSizeHandler(this, EventArgs.Empty);
-					break;
-                case Key.Plus | Key.Minus:
-                    this.HandleOpacity(e);
-                    break;
-			}
+        case Keys.ControlKey | Keys.S:
+            SetSizeHandler(this, EventArgs.Empty);
+            break;
+        case Key.Plus | Key.Minus:
+            this.HandleOpacity(e);
+            break;
+    }
 
-			base.OnKeyDown(e);
-		}
-    
+    base.OnKeyDown(e);
+}
+
 private void HandleOpacity(KeyEventArgs e)
 {
     if (e.Keycode == Key.Plus)
     {
-        rulerInfo.opacity*100;
+        rulerInfo.opacity * 100;
     }
     else
     {
-        rulerInfo.opacity/100;
+        rulerInfo.opacity / 100;
     }
 }
